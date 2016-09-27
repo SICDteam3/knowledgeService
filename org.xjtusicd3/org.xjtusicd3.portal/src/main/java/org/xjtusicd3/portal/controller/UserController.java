62322b1e8875851a3970f62852b36ae3df2cda02
sicd_happy <sicd_happy@sicd_happy-PC>
2016-09-27 13:39:45 +0800

    第一次后台comment

--- org.xjtusicd3/org.xjtusicd3.portal/src/main/java/org/xjtusicd3/portal/controller/UserController.java
+++ org.xjtusicd3/org.xjtusicd3.portal/src/main/java/org/xjtusicd3/portal/controller/UserController.java
@@ -1,5 +1,39 @@
 package org.xjtusicd3.portal.controller;
 
-public class UserController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+public class UserController {
+	
+	   @RequestMapping(value="userSearch",method=RequestMethod.GET)
+       public ModelAndView  userSearch(String name){
+    	   ModelAndView mv=new ModelAndView();
+    	   
+    	   return mv;
+       }
+	   
+	   @RequestMapping(value="userBlack",method=RequestMethod.GET)
+       public ModelAndView  userBlack(String name){
+    	   ModelAndView mv=new ModelAndView();
+    	   
+    	   return mv;
+       }
+	   
+	   @RequestMapping(value="userRecover",method=RequestMethod.GET)
+       public ModelAndView  userRecover(String name){
+    	   ModelAndView mv=new ModelAndView();
+    	   
+    	   return mv;
+       }
+	   
+	   
+	   @RequestMapping(value="userPswAlter",method=RequestMethod.GET)
+       public ModelAndView  userPswAlter(String name){
+    	   ModelAndView mv=new ModelAndView();
+    	   
+    	   return mv;
+       }
+	   
+	   
 }
