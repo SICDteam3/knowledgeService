7976bf1d5a3f794ef4b767cb7c5de6e8c25e7179
xiaofan <xiaofan@DESKTOP-RFEG6L7>
2016-09-29 22:55:24 +0800

    修改了页面不能访问的bug

--- org.xjtusicd3/org.xjtusicd3.portal/src/main/java/org/xjtusicd3/portal/controller/LogController.java
+++ org.xjtusicd3/org.xjtusicd3.portal/src/main/java/org/xjtusicd3/portal/controller/LogController.java
@@ -1,17 +1,21 @@
 package org.xjtusicd3.portal.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
+@Controller
+@RequestMapping(value="log")
 public class LogController {
      
-	
+	@RequestMapping(value="getlogpage",method=RequestMethod.GET)
+	public ModelAndView getlogPage(){
+		ModelAndView mv=new ModelAndView("logmanagement/logcheck");		
+		return mv;
+	}
 	@RequestMapping(value="userLog",method=RequestMethod.GET)
 	public ModelAndView userLog(String name,String StartTime,String EndTeme){
-		ModelAndView mv=new ModelAndView();
-		
-		
+		ModelAndView mv=new ModelAndView();		
 		return mv;
 	}
 	
--- org.xjtusicd3/org.xjtusicd3.portal/src/main/java/org/xjtusicd3/portal/controller/PatentMangmentController.java
+++ org.xjtusicd3/org.xjtusicd3.portal/src/main/java/org/xjtusicd3/portal/controller/PatentMangmentController.java
@@ -1,9 +1,11 @@
 package org.xjtusicd3.portal.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class PatentMangmentController {
     @RequestMapping(value="patentSearch",method=RequestMethod.GET)
 	public ModelAndView patentSearch(){
