933925410c42c4f1743cbb7a6082a3d55d617e96
zhaoyq <zhaoyq@zhao>
2016-09-30 10:24:33 +0800

    2016.9.30

--- org.xjtusicd3/org.xjtusicd3.partner/src/main/java/org/xjtusicd3/partner/controller/PatentController.java
+++ org.xjtusicd3/org.xjtusicd3.partner/src/main/java/org/xjtusicd3/partner/controller/PatentController.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.xjtusicd3.database.model.Page;
 import org.xjtusicd3.partner.service.PatentService;
 import org.xjtusicd3.partner.view.ViewPatent;
 import org.xjtusicd3.partner.view.ViewPatentVisual;
